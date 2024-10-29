e218a9f10daf71bb5687eeeda3a1390f6c8d91f2
OLekxanTalabir <talabirchuk.oleksandr@student.uzhnu.edu.ua>
2024-10-29 09:05:01 +0200

    my prodgect

---
+++ long 1/load.js
@@ -0,0 +1,24 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+
+
+//     setInterval(() => {
+
+
+
+//         setTimeout(() => {
+
+//             document.querySelector(".line").style.width ="100px";
+
+//         }, 250);
+
+//         setTimeout(() => {
+
+//             document.querySelector(".line").style.width = "50px";
+
+//         }, 250);
+
+
+        
+//     }, 500);
+// });
